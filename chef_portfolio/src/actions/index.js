6ac2a767cb2105dd71a551e0b8d1ae82bcecd713
Nate Magera <nmagera07@gmail.com>
2019-06-24 18:30:29 -0400

    merged changes with jacob

--- chef_portfolio/src/actions/index.js
+++ chef_portfolio/src/actions/index.js
@@ -35,19 +35,12 @@ export const login = (username, password) => dispatch => {
 export const REGISTRATION_START = 'REGISTRATION_START';
 export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
 export const REGISTRATION_FAILURE = 'REGISTRATION_FAILURE';
-<<<<<<< HEAD
 const url = 'https://chef-portfolio-bw.herokuapp.com/api'
 export const addUser = addUser => dispatch => {
   console.log(addUser)
   dispatch({ type: REGISTRATION_START });
    axios
     .post(`${url}/auth/register`, addUser)
-=======
-export const registration = addUser => dispatch => {
-  dispatch({ type: REGISTRATION_START });
-  return axios
-    .post(`${URL}/`, addUser)
->>>>>>> 8128bc6ef991e43c746efbdfe4b68fa40ef1db54
     .then(res => {
         console.log(res)
         localStorage.setItem('token', res.data.token)
