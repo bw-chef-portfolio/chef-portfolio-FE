f7b9b406aa4aebbcb9754ac447f352351d321cf2
Jengopockets <jengopockets@gmail.com>
2019-06-24 13:55:04 -0400

    started login

---
+++ chef_portfolio/src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username:'',
+            password
+        };
+    }
+    
+}
--- chef_portfolio/src/index.js
+++ chef_portfolio/src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';;
 import logger from 'redux-logger';
-import * as serviceWorker from './serviceWorker';
 import rootReducer from "./reducers";
 
 const store = createStore(
@@ -18,7 +17,4 @@ ReactDOM.render(
 </Provider>,
  document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
