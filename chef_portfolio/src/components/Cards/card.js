2f1544ee8947fc615f5080e01108c097f378c70d
Jengopockets <jengopockets@gmail.com>
2019-06-25 12:41:14 -0400

    Created Card Layout

---
+++ chef_portfolio/src/components/Cards/card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {PropTypes} from 'prop-types';
+import styled from 'styled-components';
+const Card = styled.div``;
+const Head = styled.h1``;
+const Ingredient = styled.li``;
+const Name = styled.h1``;
+const Location = styled.h2``;
+const Photo = styled.img``;
+
+const Card = (props) => {
+    return(
+        <Card key={props.card.id}>
+            <Name>{props.card.chef_name}</Name>
+            <Location>{props.card.chef_location}</Location>
+            <Head>{props.card.recipie_title}</Head>
+            <Photo>{props.card.item_photo}</Photo>            
+            <Ingredient>{props.card.item_ingredients}</Ingredient>
+        </Card>
+    )
+}
