26ecfb755577a34a4df52e2953894379d2907cc9
Daniel <danielstarling4@gmail.com>
2019-06-27 17:32:24 -0700

    whitespace

--- chef_portfolio/src/components/withAuth/ChefPage.js
+++ chef_portfolio/src/components/withAuth/ChefPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import ChefCard from '../../components/ChefCard'
-import {getData, addRecipe, deleteRecipe, editRecipe} from '../../actions'
+import { getData, addRecipe, deleteRecipe, editRecipe } from '../../actions'
 
 class ChefPage extends React.Component {
     state = {
@@ -15,25 +15,25 @@ class ChefPage extends React.Component {
         }
     };
 
-   componentDidMount() {
-       this.props.getData()
-   }
+    componentDidMount() {
+        this.props.getData()
+    }
 
-   componentDidUpdate(prevProps, prevState) {
-       if(prevProps.chefData.length !== this.props.chefData.length) {
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.chefData.length !== this.props.chefData.length) {
             this.props.getData()
-       }
-   }
-   
+        }
+    }
+
 
     handleChanges = e => {
-    this.setState({
-      chefInfo: {
-        ...this.state.chefInfo,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
+        this.setState({
+            chefInfo: {
+                ...this.state.chefInfo,
+                [ e.target.name ]: e.target.value
+            }
+        });
+    };
 
     addRecipe = e => {
         e.preventDefault()
@@ -49,76 +49,76 @@ class ChefPage extends React.Component {
         })
     }
 
-    
 
-    
 
-    
+
+
+
 
     render() {
-        console.log("help",this.props.chefData)
+        // console.log("help",this.props.chefData)
         if (!this.props.chefData) {
             return <h3>Loading</h3>
         } else {
-        return (
-            <div className="chef-page-title">
-                <h1>My Recipes</h1>
-               <ChefCard data={this.props.chefData} />
-               <h2>Add a new recipe!</h2>
-               <div className="chef-form">
-                <form>
-                    <p>Chef Name: </p>
-                     <input
-                        type="text"
-                        name="chef_name"
-                        value={this.state.chefInfo.chef_name}
-                        onChange={this.handleChanges}
-                    ></input>
-                    <p>Recipe Name: </p>
-                     <input
-                        type="text"
-                        name="recipe_title"
-                        value={this.state.chefInfo.recipe_title}
-                        onChange={this.handleChanges}
-                    ></input>
-                    <p>Upload a photo</p>
-                     <input
-                        type="text"
-                        name="item_photo"
-                        value={this.state.chefInfo.item_photo}
-                        onChange={this.handleChanges}
-                    ></input>
-                    <p>Location: </p>
-                     <input
-                        type="text"
-                        name="chef_location"
-                        value={this.state.chefInfo.chef_location}
-                        onChange={this.handleChanges}
-                    ></input>
-                    <p>Ingredients: </p>
-                     <input
-                        type="text"
-                        name="item_ingredients"
-                        value={this.state.chefInfo.item_ingredients}
-                        onChange={this.handleChanges}
-                        
-                    ></input>
-                    
-                    <button onClick={this.addRecipe}>Submit</button>
-                </form>
+            return (
+                <div className="chef-page-title">
+                    <h1>My Recipes</h1>
+                    <ChefCard data={this.props.chefData} />
+                    <h2>Add a new recipe!</h2>
+                    <div className="chef-form">
+                        <form>
+                            <p>Chef Name: </p>
+                            <input
+                                type="text"
+                                name="chef_name"
+                                value={this.state.chefInfo.chef_name}
+                                onChange={this.handleChanges}
+                            ></input>
+                            <p>Recipe Name: </p>
+                            <input
+                                type="text"
+                                name="recipe_title"
+                                value={this.state.chefInfo.recipe_title}
+                                onChange={this.handleChanges}
+                            ></input>
+                            <p>Upload a photo</p>
+                            <input
+                                type="text"
+                                name="item_photo"
+                                value={this.state.chefInfo.item_photo}
+                                onChange={this.handleChanges}
+                            ></input>
+                            <p>Location: </p>
+                            <input
+                                type="text"
+                                name="chef_location"
+                                value={this.state.chefInfo.chef_location}
+                                onChange={this.handleChanges}
+                            ></input>
+                            <p>Ingredients: </p>
+                            <input
+                                type="text"
+                                name="item_ingredients"
+                                value={this.state.chefInfo.item_ingredients}
+                                onChange={this.handleChanges}
+
+                            ></input>
+
+                            <button onClick={this.addRecipe}>Submit</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
-}
 }
 const mapStateToProps = state => {
     console.log("state", state)
     return {
-       error: state.chefPageReducer.error,
-       fetchingData: state.chefPageReducer.fetchingData,
-       chefData: state.chefPageReducer.chefData,
-       
+        error: state.chefPageReducer.error,
+        fetchingData: state.chefPageReducer.fetchingData,
+        chefData: state.chefPageReducer.chefData,
+
     }
 }
 export default connect(
